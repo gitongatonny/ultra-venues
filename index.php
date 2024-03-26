493c3afad04d438d0991e534ebb45e8103200c8f
Tonny G <118627853+tonnygitonga@users.noreply.github.com>
2024-03-26 14:52:00 +0300

    Added the base elements

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Home | Ultra Venues</title>
+
+        <!-- Head Content -->
+        <?php include "includes/head-content.php";?>
+
+
+
+    </head>
+
+    <body>
+        <!-- Header START -->
+        <?php include "includes/header-navbar.php";?>
+        <!-- Header END -->
+
+        <main>
+
+                
+        </main>
+
+        <!-- Footer START -->
+            <?php include "includes/footer.php";?>
+        <!-- Footer END -->
+    </body>
+
+
+</html>
