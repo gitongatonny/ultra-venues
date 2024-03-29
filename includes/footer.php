7255bcf83f69df762ca6b88a1ad0641ed3e3057a
Tonny G <118627853+tonnygitonga@users.noreply.github.com>
2024-03-29 03:30:02 +0300

    Finalized the main footer

--- includes/footer.php
+++ includes/footer.php
@@ -9,9 +9,10 @@ Footer START -->
 			<div class="col-lg-3">
 				<!-- logo -->
 				<a href="index.php">
-					<img class="h-40px" src="assets/images/" alt="ultra-venues-logo">
+					<img class="light-mode-item navbar-brand-item" src="assets/images/logo.svg" alt="ultra-venues-logo" style="height: 80px; width: 120px;">
+					<img class="dark-mode-item navbar-brand-item" src="assets/images/logo.svg" alt="ultra-venues-logo" style="height: 80px; width: 120px;">
 				</a>
-				<p class="my-3 text-muted">Welcome to Ultra Venues</p>
+				<p class="my-3 text-muted">Get the Best</p>
 				<p class="mb-2"><a href="#" class="text-muted text-primary-hover"><i class="bi bi-telephone me-2"></i>+254 700 000 000</a> </p>
 				<p class="mb-0"><a href="#" class="text-muted text-primary-hover"><i class="bi bi-envelope me-2"></i>info@ultravenues.com</a></p>
 			</div>
@@ -21,7 +22,7 @@ Footer START -->
 			<div class="col-lg-8 ms-auto">
 				<div class="row g-4">
 					<!-- Link block -->
-					<div class="col-6 col-md-3">
+					<div class="col-6 col-md-4">
 						<h5 class="text-white mb-2 mb-md-4">Main Pages</h5>
 						<ul class="nav flex-column text-primary-hover">
                             <li class="nav-item"><a class="nav-link text-muted" href="#">Home</a></li>
@@ -31,20 +32,10 @@ Footer START -->
 						</ul>
 					</div>
 
-					<!-- Link block -->
-					<div class="col-6 col-md-3">
-						<h5 class="text-white mb-2 mb-md-4">Links</h5>
-						<ul class="nav flex-column text-primary-hover">
-							<li class="nav-item"><a class="nav-link text-muted" href="#">Login</a></li>
-							<li class="nav-item"><a class="nav-link text-muted" href="#">Register</a></li>
-							<li class="nav-item"><a class="nav-link text-muted" href="#">Privacy Policy</a></li>
-							<li class="nav-item"><a class="nav-link text-muted" href="#">Terms & Conditions</a></li>
-						</ul>
-					</div>
 									
 					<!-- Link block -->
-					<div class="col-6 col-md-3">
-						<h5 class="text-white mb-2 mb-md-4">Locations</h5>
+					<div class="col-6 col-md-4">
+						<h5 class="text-white mb-2 mb-md-4">Top Locations</h5>
 						<ul class="nav flex-column text-primary-hover">
 							<li class="nav-item"><a class="nav-link text-muted" href="#">Nyeri</a></li>
 							<li class="nav-item"><a class="nav-link text-muted" href="#">Nanyuki</a></li>
@@ -55,12 +46,14 @@ Footer START -->
 					</div>
 
 					<!-- Link block -->
-					<div class="col-6 col-md-3">
-						<h5 class="text-white mb-2 mb-md-4">Venues</h5>
+					<div class="col-6 col-md-4">
+						<h5 class="text-white mb-2 mb-md-4">Top Event Types</h5>
 						<ul class="nav flex-column text-primary-hover">
-							<li class="nav-item"><a class="nav-link text-muted" href="#"><i class="fa-solid fa-hotel me-2"></i>Locations</a></li>
-							<li class="nav-item"><a class="nav-link text-muted" href="#"><i class="fa-solid fa-plane me-2"></i>Event Type</a></li>
-							<li class="nav-item"><a class="nav-link text-muted" href="#"><i class="fa-solid fa-globe-americas me-2"></i>Contact Us</a></li>
+							<li class="nav-item"><a class="nav-link text-muted" href="#"><i class="fa-solid fa-ring me-2"></i>Wedding</a></li>
+							<li class="nav-item"><a class="nav-link text-muted" href="#"><i class="fa-solid fa-plane me-2"></i>Staff Retreat</a></li>
+							<li class="nav-item"><a class="nav-link text-muted" href="#"><i class="fa-solid fa-briefcase me-2"></i>Corporate Meeting</a></li>
+							<li class="nav-item"><a class="nav-link text-muted" href="#"><i class="fa-solid fa-calendar-alt me-2"></i>Personal Custom Event</a></li>
+
 						</ul>
 					</div>
 				</div>
@@ -76,10 +69,9 @@ Footer START -->
 			<div class="col-sm-7 col-md-6 col-lg-4">
 				<h5 class="text-white mb-2">Payment & Security</h5>
 				<ul class="list-inline mb-0 mt-3">
-					<li class="list-inline-item"> <a href="#"><img src="assets/images/element/paypal.svg" class="h-30px" alt=""></a></li>
+					<li class="list-inline-item"> <a href="#"><img src="assets/images/element/mpesa.png" class="h-30px" alt=""></a></li>
 					<li class="list-inline-item"> <a href="#"><img src="assets/images/element/visa.svg" class="h-30px" alt=""></a></li>
 					<li class="list-inline-item"> <a href="#"><img src="assets/images/element/mastercard.svg" class="h-30px" alt=""></a></li>
-					<li class="list-inline-item"> <a href="#"><img src="assets/images/element/expresscard.svg" class="h-30px" alt=""></a></li>
 				</ul>
 			</div>
 
@@ -103,7 +95,7 @@ Footer START -->
 			<div class="container">
 				<div class="d-lg-flex justify-content-between align-items-center py-3 text-center text-lg-start">
 					<!-- copyright text -->
-					<div class="text-muted text-primary-hover"> Copyrights ©2024 Ultra-Venues. Build by <a href="#" class="text-muted">Dev-KE</a>. </div>
+					<div class="text-muted text-primary-hover"> Copyrights ©2024 Ultra-Venues. Built by <a href="#" class="text-muted">Dev-KE</a>. </div>
 					<!-- copyright links-->
 					<div class="nav mt-2 mt-lg-0">
 						<ul class="list-inline text-primary-hover mx-auto mb-0">
@@ -120,23 +112,6 @@ Footer START -->
 <!-- =======================
 Footer END -->
 
-<!-- Modal START -->
-<div class="modal fade" id="map360" aria-hidden="true">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<!-- Title -->
-			<div class="modal-header">
-				<h5 class="modal-title" id="map360label">Hotel 360 View</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-			</div>
-			<!-- Body -->
-			<div class="modal-body p-3">
-				<iframe src="https://www.google.com/maps/embed?pb=!4v1664190302197!6m8!1m7!1sgWjBbRwH2wMmQTbvyZwkGw!2m2!1d51.49712857925994!2d-0.1593322776188391!3f348.1837813715552!4f17.07463868756892!5f0.8485845663286693" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-			</div>
-		</div>
-	</div>
-</div>
-<!-- Modal END -->
 
 <!-- Back to top -->
 <div class="back-top"></div>
