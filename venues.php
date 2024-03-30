c33ada9fd35e99b0b6a901c92c48e2257f049466
Tonny G <118627853+tonnygitonga@users.noreply.github.com>
2024-03-30 09:20:35 +0300

    Added venues page

---
+++ venues.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Venues | Ultra Venues</title>
+
+        <!-- Head Content -->
+        <?php include "includes/head-content.php";?>
+
+
+
+    </head>
+
+    <body>
+        <!-- Header START -->
+        <?php include "includes/header-main.php";?>
+        <!-- Header END -->
+
+        <main>
+
+        </main>
+
+        <!-- Footer START -->
+            <?php include "includes/footer.php";?>
+        <!-- Footer END -->
+    </body>
+
+
+</html>
