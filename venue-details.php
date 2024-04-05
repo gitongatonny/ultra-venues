1020605cf938361c1a312a9a883fa3decec5a059
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-05 23:48:27 +0300

    Added the Title and Tabs section

--- venue-details.php
+++ venue-details.php
@@ -88,6 +88,66 @@
         <!-- =======================
         Main banner END -->
 
+
+<!-- =======================
+Title and tabs START -->
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="card bg-light p-0 pb-0">
+					<!-- Card body -->
+					<div class="card-body d-flex justify-content-between flex-wrap">
+						<!-- Title and badge -->
+						<div>
+							<!-- Badge -->
+							<div class="badge text-bg-dark"><i class="fa-solid fa-spa fa-fw text-warning"></i> Hotel</div>
+							<div class="badge text-bg-dark"><i class="bi bi-scissors fa-fw text-warning"></i> Nyeri</div>
+							<div class="badge text-bg-dark"><i class="fas fa-star fa-fw text-warning"></i> 4.6</div>
+							
+							<!-- Title -->
+							<h1 class="h3 mt-2 mb-1">Green Hills Hotel</h1>
+							<p class="mb-2 mb-sm-0"><i class="bi bi-geo-alt me-1 text-primary"></i>Nyeri</p>Nanyuki Highway, Chaka</p>
+						</div>
+						<!-- Buttons -->
+						<ul class="list-inline mb-0">
+							<!-- wishlist -->
+							<li class="list-inline-item">
+								<a href="#" class="btn btn-sm btn-white px-2"><i class="fa-solid fa-fw fa-heart"></i></a>
+							</li>
+							<!-- Share -->
+							<li class="list-inline-item dropdown">
+								<!-- Share button -->
+								<a href="#" class="btn btn-sm btn-white px-2" role="button" id="dropdownShare" data-bs-toggle="dropdown" aria-expanded="false">
+									<i class="fa-solid fa-fw fa-share-alt"></i>
+								</a>
+								<!-- dropdown button -->
+								<ul class="dropdown-menu min-w-auto shadow rounded" aria-labelledby="dropdownShare">
+									<li><a class="dropdown-item" href="#"><i class="fa-solid fa-copy me-2"></i>Copy link</a></li>
+								</ul>
+							</li>
+							<!-- Report -->
+							<li class="list-inline-item">
+								<a href="#" class="btn btn-sm btn-white px-2"><i class="bi bi-exclamation-triangle-fill fa-fw"></i></a>
+							</li>
+						</ul>
+					</div>
+					<!-- Card footer -->
+					<div class="card-footer bg-transparent border-top py-0">
+						<ul class="nav nav-tabs nav-bottom-line nav-responsive border-0" role="tablist">
+							<li class="nav-item"> <a class="nav-link mb-0 active" data-bs-toggle="tab" href="#tab-1">About</a> </li>
+							<li class="nav-item"> <a class="nav-link mb-0" data-bs-toggle="tab" href="#tab-2">Booking</a> </li>
+							<li class="nav-item"> <a class="nav-link mb-0" data-bs-toggle="tab" href="#tab-3">Services</a> </li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- =======================
+Title and tabs END -->
+
         </main>
 
         <!-- Footer START -->
