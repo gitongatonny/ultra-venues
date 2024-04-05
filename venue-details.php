a7c40fa260490e5bad1daa30e3f15717f993b511
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-05 23:58:51 +0300

    Added the About tab

--- venue-details.php
+++ venue-details.php
@@ -148,6 +148,258 @@ Title and tabs START -->
 <!-- =======================
 Title and tabs END -->
 
+<!-- =======================
+Content START -->
+<section class="pt-0">
+	<div class="container">
+		<!-- Tabs contents START -->
+		<div class="tab-content mb-0" id="tour-pills-tabContent">
+
+			<!-- Content item START -->
+			<div class="tab-pane fade show active" id="tab-1" role="tabpanel" aria-labelledby="tab-1">
+				<div class="row g-4 g-lg-5">
+					<!-- Left side START -->
+					<div class="col-lg-7 col-xl-8">
+						<!-- Card START -->
+						<div class="card bg-transparent mb-4">
+							<!-- Card header -->
+							<div class="card-header bg-transparent border-bottom px-0 pt-0">
+								<h4 class="mb-0">Description</h4>
+							</div>
+                            <!-- Card body -->
+                            <div class="card-body px-0 pb-0">
+                                <p class="mb-3">Green Hills Hotel is a luxurious hotel located in the beautiful city of Nyeri, Kenya. With its stunning architecture and breathtaking views, it offers a truly unforgettable experience for its guests. The hotel boasts spacious and elegantly designed rooms, equipped with modern amenities to ensure a comfortable stay. Guests can indulge in a wide range of facilities and services, including a swimming pool, spa, and fitness center. The hotel's restaurant serves delicious local and international cuisine, prepared by skilled chefs. Whether you're traveling for business or leisure, Green Hills Hotel is the perfect choice for a memorable stay in Nyeri.</p>
+
+                                <p class="mb-0">Experience the warmth and hospitality of the staff, who are dedicated to providing exceptional service and making your stay truly enjoyable. Explore the nearby attractions, such as Mount Kenya National Park and Aberdare National Park, or simply relax and unwind in the hotel's serene surroundings. Green Hills Hotel is the ideal destination for those seeking luxury, comfort, and tranquility in the heart of Nyeri, Kenya.</p>
+                            </div>
+						</div>
+						<!-- Card END -->
+
+						<!-- Card START -->
+						<div class="card bg-transparent">
+							<!-- Card header -->
+							<div class="card-header bg-transparent border-bottom px-0">
+								<h4 class="mb-0">Image Gallery</h4>
+							</div>
+							<!-- Card body -->
+							<div class="card-body px-0 pb-0">
+								<div class="row g-4">
+									<div class="col-md-4">
+										<a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
+											<div class="card card-element-hover card-overlay-hover overflow-hidden">
+												<!-- Image -->
+												<img src="assets/images/mt-kenya.jpg" class="card-img" alt="">
+												<!-- Full screen button -->
+												<div class="hover-element w-100 h-100">
+													<i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+												</div>
+											</div>
+										</a>
+									</div>
+
+									<div class="col-md-4">
+										<a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
+											<div class="card card-element-hover card-overlay-hover overflow-hidden">
+												<!-- Image -->
+												<img src="assets/images/mt-kenya.jpg" class="card-img" alt="">
+												<!-- Full screen button -->
+												<div class="hover-element w-100 h-100">
+													<i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+												</div>
+											</div>
+										</a>
+									</div>
+
+									<div class="col-md-4">
+										<a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
+											<div class="card card-element-hover card-overlay-hover overflow-hidden">
+												<!-- Image -->
+												<img src="assets/images/mt-kenya.jpg" class="card-img" alt="">
+												<!-- Full screen button -->
+												<div class="hover-element w-100 h-100">
+													<i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+												</div>
+											</div>
+										</a>
+									</div>
+
+									<div class="col-md-4">
+										<a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
+											<div class="card card-element-hover card-overlay-hover overflow-hidden">
+												<!-- Image -->
+												<img src="assets/images/mt-kenya.jpg" class="card-img" alt="">
+												<!-- Full screen button -->
+												<div class="hover-element w-100 h-100">
+													<i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+												</div>
+											</div>
+										</a>
+									</div>
+
+									<div class="col-md-4">
+										<a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
+											<div class="card card-element-hover card-overlay-hover overflow-hidden">
+												<!-- Image -->
+												<img src="assets/images/mt-kenya.jpg" class="card-img" alt="">
+												<!-- Full screen button -->
+												<div class="hover-element w-100 h-100">
+													<i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+												</div>
+											</div>
+										</a>
+									</div>
+
+									<div class="col-md-4">
+										<a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
+											<div class="card card-element-hover card-overlay-hover overflow-hidden">
+												<!-- Image -->
+												<img src="assets/images/mt-kenya.jpg" class="card-img" alt="">
+												<!-- Full screen button -->
+												<div class="hover-element w-100 h-100">
+													<i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+												</div>
+											</div>
+										</a>
+									</div>
+
+									<div class="col-md-4">
+										<a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
+											<div class="card card-element-hover card-overlay-hover overflow-hidden">
+												<!-- Image -->
+												<img src="assets/images/mt-kenya.jpg" class="card-img" alt="">
+												<!-- Full screen button -->
+												<div class="hover-element w-100 h-100">
+													<i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+												</div>
+											</div>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Card END -->
+					</div>
+					<!-- Left side END -->
+
+					<!-- Right side START -->
+					<div class="col-lg-5 col-xl-4">
+						<!-- Direction START -->
+						<div class="card shadow mb-4">
+							<!-- Card header -->
+							<div class="card-header border-bottom">
+								<h5 class="mb-0">Get Direction</h5>
+							</div>
+
+							<!-- Card body -->
+							<div class="card-body">
+								<!-- Map START -->
+								<iframe class="w-100 grayscale rounded" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.9663095343008!2d-74.00425878428698!3d40.74076684379132!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259bf5c1654f3%3A0xc80f9cfce5383d5d!2sGoogle!5e0!3m2!1sen!2sin!4v1586000412513!5m2!1sen!2sin" height="200" style="border:0;" aria-hidden="false" tabindex="0"></iframe>
+								
+								<!-- Info -->
+								<ul class="list-group list-group-borderless my-3">
+									<!-- Address -->
+									<li class="list-group-item">
+										<a href="#" class="h6 fw-normal mb-0">
+											<i class="bi fa-fw bi-geo-alt me-2"></i>Nanyuki Highway, Chaka
+										</a>
+									</li>
+									<!-- Call no -->
+									<li class="list-group-item">
+										<a href="#" class="h6 fw-normal mb-0">
+											<i class="bi fa-fw bi-telephone-forward me-2"></i>+254 123 456 789
+										</a>
+									</li>
+									<!-- Email -->
+									<li class="list-group-item">
+										<a href="#" class="h6 fw-normal mb-0">
+											<i class="bi fa-fw bi-envelope me-2"></i>info@greenhillshotel.com
+										</a>
+									</li>
+								</ul>
+								<!-- Social button -->
+								<ul class="list-inline mb-0 mt-3">
+									<li class="list-inline-item">Follow us on: </li>
+									<li class="list-inline-item"> <a class="btn btn-sm px-2 bg-facebook mb-0" href="#"><i class="fab fa-fw fa-facebook-f"></i></a> </li>
+									<li class="list-inline-item"> <a class="btn btn-sm shadow px-2 bg-instagram mb-0" href="#"><i class="fab fa-fw fa-instagram"></i></a> </li>
+								</ul>
+							</div>
+						</div>
+						<!-- Direction END -->
+
+						<!-- Time table START -->
+						<div class="card shadow">
+							<!-- Card header -->
+							<div class="card-header border-bottom">
+								<h5 class="mb-0">Working Hours</h5>
+							</div>
+
+							<!-- Card body -->
+							<div class="card-body">
+								<ul class="list-group list-group-borderless mb-0">
+									<li class="list-group-item d-flex justify-content-between">
+										<span>Monday</span>
+										<span>9:00 AM to 8:00 PM</span>
+									</li>
+
+									<li class="list-group-item py-0"><hr class="my-1"></li>
+
+									<li class="list-group-item d-flex justify-content-between">
+										<span>Tuesday</span>
+										<span>9:00 AM to 8:00 PM</span>
+									</li>
+
+									<li class="list-group-item py-0"><hr class="my-1"></li>
+
+									<li class="list-group-item d-flex justify-content-between">
+										<span>Wednesday</span>
+										<span>9:00 AM to 8:00 PM</span>
+									</li>
+
+									<li class="list-group-item py-0"><hr class="my-1"></li>
+
+									<li class="list-group-item d-flex justify-content-between">
+										<span>Thursday</span>
+										<span>9:00 AM to 8:00 PM</span>
+									</li>
+
+									<li class="list-group-item py-0"><hr class="my-1"></li> 
+
+									<li class="list-group-item d-flex justify-content-between">
+										<span>Friday</span>
+										<span>9:00 AM to 8:00 PM</span>
+									</li>
+
+									<li class="list-group-item py-0"><hr class="my-1"></li> 
+
+									<li class="list-group-item d-flex justify-content-between">
+										<span>Saturday</span>
+										<span>Open all day</span>
+									</li>
+
+									<li class="list-group-item py-0"><hr class="my-1"></li> 
+
+									<li class="list-group-item d-flex justify-content-between">
+										<span>Sunday</span>
+										<span>Open all day</span>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<!-- Time table END -->
+					</div>
+					<!-- Right side END -->
+				</div>
+			</div>
+			<!-- Content item END -->
+
+
+		</div>
+		<!-- Tabs contents END -->		
+	</div>
+</section>
+<!-- =======================
+Content END -->
+
         </main>
 
         <!-- Footer START -->
