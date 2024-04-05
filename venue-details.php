1ec02c7c14d41b87d708d046445738fa7b3f4370
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-05 23:42:49 +0300

    Added the main banner

--- venue-details.php
+++ venue-details.php
@@ -18,6 +18,76 @@
 
         <main>
 
+        <!-- =======================
+        Main banner START -->
+        <section class="py-0">
+            <div class="container-fluid px-0">
+                <!-- Slider START -->
+                <div class="tiny-slider arrow-round arrow-blur">
+                    <div class="tiny-slider-inner" data-autoplay="true" data-arrow="true" data-gutter="0" data-edge="0" data-dots="false" data-items="3" data-items-md="2" data-items-xs="1">
+                        <!-- Slider item -->
+                        <div>
+                            <a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
+                                <div class="card card-element-hover card-overlay-hover rounded-0 overflow-hidden">
+                                    <!-- Image -->
+                                    <img src="assets/images/mt-kenya.jpg" class="" alt="">
+                                    <!-- Full screen button -->
+                                    <div class="hover-element w-100 h-100">
+                                        <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <!-- Slider item -->
+                        <div>
+                            <a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/Locations/Nyeri.jpg">
+                                <div class="card card-element-hover card-overlay-hover rounded-0 overflow-hidden">
+                                    <!-- Image -->
+                                    <img src="assets/images/Locations/Nyeri.jpg" class="" alt="">
+                                    <!-- Full screen button -->
+                                    <div class="hover-element w-100 h-100">
+                                        <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <!-- Slider item -->
+                        <div>
+                            <a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
+                                <div class="card card-element-hover card-overlay-hover rounded-0 overflow-hidden">
+                                    <!-- Image -->
+                                    <img src="assets/images/mt-kenya.jpg" class="" alt="">
+                                    <!-- Full screen button -->
+                                    <div class="hover-element w-100 h-100">
+                                        <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                        <!-- Slider item -->
+                        <div>
+                            <a class="w-100 h-100" data-glightbox data-gallery="gallery" href="aassets/images/Locations/Nyeri.jpg">
+                                <div class="card card-element-hover card-overlay-hover rounded-0 overflow-hidden">
+                                    <!-- Image -->
+                                    <img src="assets/images/Locations/Nyeri.jpg" class="" alt="">
+                                    <!-- Full screen button -->
+                                    <div class="hover-element w-100 h-100">
+                                        <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Slider START -->
+            </div>
+        </section>
+        <!-- =======================
+        Main banner END -->
+
         </main>
 
         <!-- Footer START -->
