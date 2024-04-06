f614fcdb0c9c0ef342838ccfacc493af07a19116
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-06 03:12:01 +0300

    Added the map modal

--- location-detail.php
+++ location-detail.php
@@ -19,130 +19,153 @@
         <main>
 
 
-<!-- =======================
-Main Title START -->
-<section class="py-0 pt-sm-5">
-	<div class="container position-relative">
-		<!-- Title and button START -->
-		<div class="row mb-3">
-			<div class="col-12">
-				<!-- Meta -->
-				<div class="d-lg-flex justify-content-lg-between mb-1">
-					<!-- Title -->
-					<div class="mb-2 mb-lg-0">
-						<h1 class="fs-2">Nyeri </h1>
-						<!-- Location -->
-						<p class="fw-bold mb-0"><i class="bi bi-geo-alt me-2"></i>Mt. Kenya Region
-							<a href="#" class="ms-2 text-decoration-underline" data-bs-toggle="modal" data-bs-target="#mapmodal">
-								<i class="bi bi-eye-fill me-1"></i>View On Map
-							</a>
-						</p>
-					</div>
+        <!-- =======================
+        Main Title START -->
+        <section class="py-0 pt-sm-5">
+            <div class="container position-relative">
+                <!-- Title and button START -->
+                <div class="row mb-3">
+                    <div class="col-12">
+                        <!-- Meta -->
+                        <div class="d-lg-flex justify-content-lg-between mb-1">
+                            <!-- Title -->
+                            <div class="mb-2 mb-lg-0">
+                                <h1 class="fs-2">Nyeri </h1>
+                                <!-- Location -->
+                                <p class="fw-bold mb-0"><i class="bi bi-geo-alt me-2"></i>Mt. Kenya Region
+                                    <a href="#" class="ms-2 text-decoration-underline" data-bs-toggle="modal" data-bs-target="#mapmodal">
+                                        <i class="bi bi-eye-fill me-1"></i>View On Map
+                                    </a>
+                                </p>
+                            </div>
 
-					<!-- Buttons -->
-					<ul class="list-inline text-end">
-						<!-- Heart icon -->
-						<li class="list-inline-item">
-							<a href="#" class="btn btn-sm btn-light px-2"><i class="fa-solid fa-fw fa-heart"></i></a>
-						</li>
-						<!-- Share icon -->
-						<li class="list-inline-item dropdown">
-							<!-- Share button -->
-							<a href="#" class="btn btn-sm btn-light px-2" role="button" id="dropdownShare" data-bs-toggle="dropdown" aria-expanded="false">
-								<i class="fa-solid fa-fw fa-share-alt"></i>
-							</a>
-							<!-- dropdown button -->
-							<ul class="dropdown-menu dropdown-menu-end min-w-auto shadow rounded" aria-labelledby="dropdownShare">
-								<li><a class="dropdown-item" href="#"><i class="fa-solid fa-copy me-2"></i>Copy link</a></li>
-							</ul>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<!-- Title and button END -->
+                            <!-- Buttons -->
+                            <ul class="list-inline text-end">
+                                <!-- Heart icon -->
+                                <li class="list-inline-item">
+                                    <a href="#" class="btn btn-sm btn-light px-2"><i class="fa-solid fa-fw fa-heart"></i></a>
+                                </li>
+                                <!-- Share icon -->
+                                <li class="list-inline-item dropdown">
+                                    <!-- Share button -->
+                                    <a href="#" class="btn btn-sm btn-light px-2" role="button" id="dropdownShare" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i class="fa-solid fa-fw fa-share-alt"></i>
+                                    </a>
+                                    <!-- dropdown button -->
+                                    <ul class="dropdown-menu dropdown-menu-end min-w-auto shadow rounded" aria-labelledby="dropdownShare">
+                                        <li><a class="dropdown-item" href="#"><i class="fa-solid fa-copy me-2"></i>Copy link</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- Title and button END -->
 
 
-	</div>
-</section>
-<!-- =======================
-Main Title END -->
+            </div>
+        </section>
+        <!-- =======================
+        Main Title END -->
 
-<!-- =======================
-Image gallery START -->
-<section class="card-grid pt-0">
-	<div class="container">
-		<div class="row g-2">
-			<!-- Image -->
-			<div class="col-md-6">
-				<a data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
-					<div class="card card-grid-lg card-element-hover card-overlay-hover overflow-hidden" style="background-image:url(assets/images/mt-kenya.jpg); background-position: center left; background-size: cover;">
-						<!-- Card hover element -->
-						<div class="hover-element position-absolute w-100 h-100">
-							<i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
-						</div>
-					</div>
-				</a>
-			</div>
+        <!-- =======================
+        Image gallery START -->
+        <section class="card-grid pt-0">
+            <div class="container">
+                <div class="row g-2">
+                    <!-- Image -->
+                    <div class="col-md-6">
+                        <a data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
+                            <div class="card card-grid-lg card-element-hover card-overlay-hover overflow-hidden" style="background-image:url(assets/images/mt-kenya.jpg); background-position: center left; background-size: cover;">
+                                <!-- Card hover element -->
+                                <div class="hover-element position-absolute w-100 h-100">
+                                    <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
 
-			<div class="col-md-6">
-				<!-- Card item START -->
-				<div class="row g-2"> 
-					<!-- Image -->
-					<div class="col-12">
-						<a data-glightbox data-gallery="gallery" href="assets/images/Locations/Nyeri.jpg">
-							<div class="card card-grid-sm card-element-hover card-overlay-hover overflow-hidden" style="background-image:url(assets/images/Locations/Nyeri.jpg); background-position: center left; background-size: cover;">
-								<!-- Card hover element -->
-								<div class="hover-element position-absolute w-100 h-100">
-									<i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
-								</div>
-							</div>
-						</a>	
-					</div>
+                    <div class="col-md-6">
+                        <!-- Card item START -->
+                        <div class="row g-2"> 
+                            <!-- Image -->
+                            <div class="col-12">
+                                <a data-glightbox data-gallery="gallery" href="assets/images/Locations/Nyeri.jpg">
+                                    <div class="card card-grid-sm card-element-hover card-overlay-hover overflow-hidden" style="background-image:url(assets/images/Locations/Nyeri.jpg); background-position: center left; background-size: cover;">
+                                        <!-- Card hover element -->
+                                        <div class="hover-element position-absolute w-100 h-100">
+                                            <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                        </div>
+                                    </div>
+                                </a>	
+                            </div>
 
-					<!-- Image -->
-					<div class="col-md-6">
-						<a data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
-							<div class="card card-grid-sm card-element-hover card-overlay-hover overflow-hidden" style="background-image:url(assets/images/mt-kenya.jpg); background-position: center left; background-size: cover;">
-								<!-- Card hover element -->
-								<div class="hover-element position-absolute w-100 h-100">
-									<i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
-								</div>
-							</div>
-						</a>	
-					</div>
+                            <!-- Image -->
+                            <div class="col-md-6">
+                                <a data-glightbox data-gallery="gallery" href="assets/images/mt-kenya.jpg">
+                                    <div class="card card-grid-sm card-element-hover card-overlay-hover overflow-hidden" style="background-image:url(assets/images/mt-kenya.jpg); background-position: center left; background-size: cover;">
+                                        <!-- Card hover element -->
+                                        <div class="hover-element position-absolute w-100 h-100">
+                                            <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                        </div>
+                                    </div>
+                                </a>	
+                            </div>
 
-					<!-- Images -->
-					<div class="col-md-6">
-						<div class="card card-grid-sm overflow-hidden" style="background-image:url(assets/images/Locations/Nyeri.jpg); background-position: center left; background-size: cover;">
-							<!-- Background overlay -->
-							<div class="bg-overlay bg-dark opacity-7"></div>
+                            <!-- Images -->
+                            <div class="col-md-6">
+                                <div class="card card-grid-sm overflow-hidden" style="background-image:url(assets/images/Locations/Nyeri.jpg); background-position: center left; background-size: cover;">
+                                    <!-- Background overlay -->
+                                    <div class="bg-overlay bg-dark opacity-7"></div>
 
-							<!-- Popup Images -->
-							<a data-glightbox="" data-gallery="gallery" href="assets/images/mt-kenya.jpg" class="stretched-link z-index-9"></a>
-							<a data-glightbox="" data-gallery="gallery" href="assets/images/Locations/Nyeri.jpg"></a>
-							<a data-glightbox="" data-gallery="gallery" href="assets/images/mt-kenya.jpg"></a>
+                                    <!-- Popup Images -->
+                                    <a data-glightbox="" data-gallery="gallery" href="assets/images/mt-kenya.jpg" class="stretched-link z-index-9"></a>
+                                    <a data-glightbox="" data-gallery="gallery" href="assets/images/Locations/Nyeri.jpg"></a>
+                                    <a data-glightbox="" data-gallery="gallery" href="assets/images/mt-kenya.jpg"></a>
 
-							<!-- Overlay text -->
-							<div class="card-img-overlay d-flex h-100 w-100">
-								<h6 class="card-title m-auto fw-light text-decoration-underline"><a href="#" class="text-white">View all</a></h6>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- Card item END -->
-			</div>
-		</div>
-	</div>
-</section>
-<!-- =======================
-Image gallery END -->
+                                    <!-- Overlay text -->
+                                    <div class="card-img-overlay d-flex h-100 w-100">
+                                        <h6 class="card-title m-auto fw-light text-decoration-underline"><a href="#" class="text-white">View all</a></h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Card item END -->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- =======================
+        Image gallery END -->
 
         </main>
 
         <!-- Footer START -->
             <?php include "includes/footer.php";?>
         <!-- Footer END -->
+
+        <!-- Map modal START -->
+        <div class="modal fade" id="mapmodal" tabindex="-1" aria-labelledby="mapmodalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+                <div class="modal-content">
+                    <!-- Title -->
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="mapmodalLabel">View the Location Area</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <!-- Map -->
+                    <div class="modal-body p-0">
+                        <iframe class="w-100" height="400" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31917.605763308555!2d36.932101652261515!3d-0.4399118570035528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x18285dfed09e0ebf%3A0x829716f702f65e9e!2sNyeri!5e0!3m2!1sen!2ske!4v1712362123253!5m2!1sen!2ske"  style="border:0;" aria-hidden="false" tabindex="0"></iframe>	
+                    </div>
+                    <!-- Button -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-sm btn-primary mb-0"><i class="bi fa-fw bi-pin-map-fill me-2"></i>View In Google Map</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Map modal END -->
+
     </body>
 
 
