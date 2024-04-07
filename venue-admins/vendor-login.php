ed21d3e0afacf8dc30f621511545db1af968e509
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-07 17:46:08 +0300

    Added venue admin login page

---
+++ venue-admins/vendor-login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Login | Ultra Venues</title>
+
+        <!-- Head Content -->
+        <?php include "includes/head-content.php";?>
+
+
+
+    </head>
+
+    <body>
+        <!-- Header START -->
+        <?php include "includes/header-main.php";?>
+        <!-- Header END -->
+
+        <main>
+                
+        </main>
+
+        <!-- Footer START -->
+            <?php include "includes/footer.php";?>
+        <!-- Footer END -->
+
+		<script>
+			function redirectToDash() {
+			window.location.href = "dash.php";
+			}
+		</script>
+
+    </body>
+
+
+</html>
