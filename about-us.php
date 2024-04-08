aef3480bd35ea1e3d50c5b14926aa17a3a7368ac
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-08 21:56:39 +0300

    Added the main banner

--- about-us.php
+++ about-us.php
@@ -18,6 +18,65 @@
 
         <main>
 
+            <!-- =======================
+            Main banner START -->
+            <section>
+                <div class="container">
+                    <div class="row mb-5">
+                        <div class="col-xl-10 mx-auto text-center">
+                            <!-- Title -->
+                            <h1>Discover the Perfect Venue for Your Next Event</h1>
+                            <p class="lead">Whether you're planning a wedding, a company meeting, or any other personal or corporate event, we have a wide selection of hotels, resorts, gardens, and more to choose from. Let us help you find the perfect venue to make your event unforgettable.</p>
+                            <!-- Meta -->
+                            <div class="hstack gap-3 flex-wrap justify-content-center">
+                                <!-- Item -->
+                                <h6 class="bg-mode shadow rounded-2 fw-normal d-inline-block py-2 px-4">
+                                    <img src="assets/images/element/06.svg" class="h-20px me-2" alt="">
+                                    1K+ Venues
+                                </h6>
+
+                                <!-- Item -->
+                                <h6 class="bg-mode shadow rounded-2 fw-normal d-inline-block py-2 px-4">
+                                    <img src="assets/images/element/07.svg" class="h-20px me-2" alt="">
+                                    10+ Locations
+                                </h6>
+
+                                <!-- Item -->
+                                <h6 class="bg-mode shadow rounded-2 fw-normal d-inline-block py-2 px-4">
+                                    <img src="assets/images/element/08.svg" class="h-20px me-2" alt="">
+                                    50+ Amenity Types Offered
+                                </h6>
+                            </div>
+                        </div>
+                    </div> <!-- Row END -->
+
+                    <!-- Image START -->
+                    <div class="row g-4 align-items-center">
+                        <!-- Image -->
+                        <div class="col-md-6">
+                            <img src="assets/images/mt-kenya.jpg" class="rounded-3" alt="">
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="row g-4">
+                                <!-- Image -->
+                                <div class="col-md-8">
+                                    <img src="assets/images/Locations/Nyeri.jpg" class="rounded-3" alt="">
+                                </div>
+
+                                <!-- Image -->
+                                <div class="col-12">
+                                    <img src="assets/images/mt-kenya.jpg" class="rounded-3" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Image END -->
+                </div>
+            </section>
+            <!-- =======================
+            Main banner START -->
+
         </main>
 
         <!-- Footer START -->
