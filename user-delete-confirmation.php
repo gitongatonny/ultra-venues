1f232c39bb24a4b8ca3b8a1a1e79cd4611098815
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-09 15:42:56 +0300

    Added the delete confirmation content

--- user-delete-confirmation.php
+++ user-delete-confirmation.php
@@ -18,6 +18,27 @@
 
         <main>
 
+            <!-- =======================
+            Main banner START -->
+            <section class="p-xl-0 mb-0">
+                <div class="container">
+                    <div class="row d-flex justify-content-center align-items-center text-center vh-100">
+                        <div class="col-xl-8">
+                            <!-- Title -->
+                            <h6 class="text-primary">We're Sad to See you Leave😢</h6>
+                            <h1>Your Account has been Successfully Deleted</h1>
+                            <p class="mb-0">Your data has been deleted from our databases. You'll need to open a new account to use our platform in the future.</p>
+
+                            <!-- Image -->
+				            <img src="assets/images/logo.svg" class="h-sm-200px h-xxl-300px mt-4" alt="">
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- =======================
+            Main banner END -->
+
         </main>
 
         <!-- Footer START -->
