172e59fe5f83a5312128694116958d8348623fd2
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-09 13:26:16 +0300

    Added the FAQs accordion section

--- faq.php
+++ faq.php
@@ -18,6 +18,148 @@
 
         <main>
 
+            <!-- =======================
+            FAQs START -->        
+            <section class="pt-8 pt-md-5">
+            <div class="container">
+                <!-- Title -->
+                <div class="row mb-4">
+                    <div class="col-12 text-center">
+                        <h2>Frequently Asked Questions</h2>
+                    </div>
+                </div>
+                <br>
+                <br>
+
+                <!-- Accordion -->
+                <div class="accordion" id="accordionFAQs">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <b>How does pricing work on Ultra-Venues?</b>
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionFAQs">
+                            <div class="accordion-body" style="color: black;">
+                                Our venue pricing varies by location, amenities, and event size, and is quoted in Kenyan Shillings (KES). We strive to offer competitive rates and occasional discounts. Prices include all applicable taxes, but please check each venue's page for any additional costs that might apply.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                <b>How can I book a venue on Ultra-Venues?</b>
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionFAQs">
+                            <div class="accordion-body" style="color: black;">
+                                Booking is made simple on Ultra-Venues. Browse or search for your preferred venue in the Mt. Kenya region, select your dates, and follow the prompt to reserve your venue. You'll receive a confirmation email with your booking details and next steps.
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFour">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                <b>Can I visit a venue before booking?</b>
+                            </button>
+                        </h2>
+                        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionFAQs">
+                            <div class="accordion-body" style="color: black;">
+                                Yes, most venues offer the option for a pre-booking visit. Please contact the venue directly through the contact information provided on our site to arrange a visit.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFive">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                <b>Are there any additional fees for venue setup and decoration?</b>
+                            </button>
+                        </h2>
+                        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionFAQs">
+                            <div class="accordion-body" style="color: black;">
+                                Some venues may charge additional fees for setup, decoration, and use of their in-house event management and catering services. These fees are listed on each venue's page under the "Additional Services" section.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingSix">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                <b>What types of events can I host with Ultra-Venues?</b>
+                            </button>
+                        </h2>
+                        <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionFAQs">
+                            <div class="accordion-body" style="color: black;">
+                                Ultra-Venues caters to a wide range of events, including weddings, graduation parties, company seminars, team building retreats, and much more. Each venue lists the types of events they are equipped to host.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingSeven">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                                <b>How do I contact a venue for specific questions?</b>
+                            </button>
+                        </h2>
+                        <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionFAQs">
+                            <div class="accordion-body" style="color: black;">
+                                For specific inquiries or requests, you can contact the venue directly using the contact information provided on the venue's page on our platform.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingEight">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                                <b>Do you offer any packages for corporate events?</b>
+                            </button>
+                        </h2>
+                        <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight" data-bs-parent="#accordionFAQs">
+                            <div class="accordion-body" style="color: black;">
+                                Yes, many of our venues offer packages specifically designed for corporate events, including conferences, seminars, and team-building retreats. These packages often include catering, AV equipment, and event coordination services.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingNine">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+                                <b>What is the booking confirmation process?</b>
+                            </button>
+                        </h2>
+                        <div id="collapseNine" class="accordion-collapse collapse" aria-labelledby="headingNine" data-bs-parent="#accordionFAQs">
+                            <div class="accordion-body" style="color: black;">
+                                After you complete your booking, you will receive an email confirmation with the venue details and your booking reference number. We recommend keeping this confirmation for your records and bringing it with you on the day of your event.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTen">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
+                                <b>Can I request additional services after booking?</b>
+                            </button>
+                        </h2>
+                        <div id="collapseTen" class="accordion-collapse collapse" aria-labelledby="headingTen" data-bs-parent="#accordionFAQs">
+                            <div class="accordion-body" style="color: black;">
+                                Yes, additional services such as catering, equipment rental, and decoration can often be added after booking. Please contact the venue directly to discuss your needs and any associated costs.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section>
+            <br>
+            <br>
+
+            <!-- =======================
+            FAQs END -->
+
+
         </main>
 
         <!-- Footer START -->
