e8aa41c1f0925e97db8e8d32199a1f86cbbd6040
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-09 15:30:54 +0300

    Done with the vendor reg thank you page

--- vendor-reg-confirm.php
+++ vendor-reg-confirm.php
@@ -18,6 +18,26 @@
 
         <main>
 
+<!-- =======================
+Main banner START -->
+<section class="p-xl-0">
+	<div class="container">
+		<div class="row d-flex justify-content-center align-items-center text-center vh-100">
+			<div class="col-xl-8">
+				<!-- Title -->
+				<h6 class="text-primary">Thank You</h6>
+				<h1>Your Venue Registration Request has been Received</h1>
+				<p class="mb-0">We'll reach out to you in 2 business days to confirm adding of your venue to our platform</p>
+
+				<!-- Image -->
+				<img src="assets/images/logo.svg" class="h-sm-300px h-xxl-400px mt-4" alt="">
+			</div>
+		</div>
+	</div>
+</section>
+<!-- =======================
+Main banner END -->
+
         </main>
 
         <!-- Footer START -->
