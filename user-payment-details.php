afebc00654ce726e55b90e2d3aa427408b32c868
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-09 14:30:07 +0300

    Added the user payment details page

---
+++ user-payment-details.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>My Payment Details | Ultra Venues</title>
+
+        <!-- Head Content -->
+        <?php include "includes/head-content.php";?>
+
+
+
+    </head>
+
+    <body>
+        <!-- Header START -->
+        <?php include "includes/header-main.php";?>
+        <!-- Header END -->
+
+        <main>
+
+<!-- =======================
+Content START -->
+<section class="pt-3">
+	<div class="container">
+		<div class="row">
+
+        <!-- Profile Sidebar START -->
+        <?php include "includes/user-profile-sidebar.php";?>
+        <!-- Profile Sidebar END -->
+
+
+
+
+
+
+        </div>
+	</div>
+</section>
+<!-- =======================
+Content END -->
+
+
+        </main>
+
+        <!-- Footer START -->
+            <?php include "includes/footer.php";?>
+        <!-- Footer END -->
+    </body>
+
+
+</html>
