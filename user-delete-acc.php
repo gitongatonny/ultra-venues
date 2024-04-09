1aa7216819f198649f0359a78f1841d58ca0bba8
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-09 15:00:42 +0300

    Added delete my account section

--- user-delete-acc.php
+++ user-delete-acc.php
@@ -29,7 +29,41 @@ Content START -->
         <?php include "includes/user-profile-sidebar.php";?>
         <!-- Profile Sidebar END -->
 
+			<!-- Main content START -->
+			<div class="col-lg-8 col-xl-9">
+				<!-- Offcanvas menu button -->
+				<div class="d-grid mb-0 d-lg-none w-100">
+					<button class="btn btn-primary mb-4" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasSidebar" aria-controls="offcanvasSidebar">
+						<i class="fas fa-sliders-h"></i> Menu
+					</button>
+				</div>
 
+				<!-- Card START -->
+				<div class="card border">
+					<!-- Card header -->
+					<div class="card-header border-bottom">
+						<h4 class="card-header-title">Delete My Account</h4>
+					</div>
+
+					<!-- Card body START -->
+					<div class="card-body">
+						<h6>Before you go...</h6>
+						<ul>
+							<li>If any issues lead to this, kindly visit our FAQs section for help first <a href="#">FAQs Page</a> </li>
+							<li>If you delete your account, you will lose your all data.</li>
+						</ul>
+						<div class="form-check form-check-md my-4">
+							<input class="form-check-input" type="checkbox" value="" id="deleteaccountCheck">
+							<label class="form-check-label" for="deleteaccountCheck">Yes, I'd like to delete my account</label>
+						</div>
+						<a href="#" class="btn btn-success-soft btn-sm mb-2 mb-sm-0">Keep my account</a>
+						<a href="#" class="btn btn-danger btn-sm mb-0">Delete my account</a>
+					</div>
+					<!-- Card body END -->
+				</div>
+				<!-- Card END -->
+			</div>
+			<!-- Main content END -->
 
 
 
