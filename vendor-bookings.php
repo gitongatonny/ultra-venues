6a1476cec505dfbd9afe431d4e1f66e1583db441
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-11 00:10:00 +0300

    Done with the vendor bookings page

--- vendor-bookings.php
+++ vendor-bookings.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
     <head>
-        <title>Vendor Dash | Ultra Venues</title>
+        <title>Vendor Bookings | Ultra Venues</title>
 
         <!-- Head Content -->
         <?php include "includes/head-content.php";?>
@@ -73,7 +73,145 @@ Menu item START -->
 <!-- =======================
 Menu item END -->
 
-	
+<!-- =======================
+Content START -->
+<section class="pt-0">
+	<div class="container vstack gap-4">
+		<!-- Title START -->
+		<div class="row">
+			<div class="col-12">
+				<h1 class="fs-4 mb-0"><i class="bi bi-bookmark-heart fa-fw me-1"></i>Bookings</h1>
+			</div>
+		</div>
+		<!-- Title END -->
+
+		<!-- Booking table START -->
+		<div class="row">
+			<div class="col-12">
+				<div class="card border">
+					<!-- Card header START -->
+					<div class="card-header border-bottom">
+						<h5 class="card-header-title">Clients<span class="badge bg-primary bg-opacity-10 text-primary ms-2">3</span></h5>
+					</div>
+					<!-- Card header END -->
+
+					<!-- Card body START -->
+					<div class="card-body">
+						<!-- Search and select START -->
+						<div class="row g-3 align-items-center justify-content-between mb-3">
+							<!-- Search -->
+							<div class="col-md-8">
+								<form class="rounded position-relative">
+									<input class="form-control pe-5" type="search" placeholder="Search" aria-label="Search">
+									<button class="btn border-0 px-3 py-0 position-absolute top-50 end-0 translate-middle-y" type="submit"><i class="fas fa-search fs-6"></i></button>
+								</form>
+							</div>
+
+							<!-- Select option -->
+							<div class="col-md-3">
+								<!-- Short by filter -->
+								<form>
+									<select class="form-select js-choice" aria-label=".form-select-sm">
+										<option value="">Sort by</option>
+										<option>Newest</option>
+										<option>Oldest</option>
+									</select>
+								</form>
+							</div>
+						</div>
+						<!-- Search and select END -->
+
+						<!-- Hotel room list START -->
+						<div class="table-responsive border-0">
+							<table class="table align-middle p-4 mb-0 table-hover table-shrink">
+								<!-- Table head -->
+								<thead class="table-light">
+									<tr>
+										<th scope="col" class="border-0 rounded-start">#</th>
+										<th scope="col" class="border-0">Name</th>
+										<th scope="col" class="border-0">Type</th>
+										<th scope="col" class="border-0">Date</th>
+										<th scope="col" class="border-0">Status</th>
+										<th scope="col" class="border-0">Payment</th>
+										<th scope="col" class="border-0 rounded-end">Action</th>
+									</tr>
+								</thead>
+
+								<!-- Table body START -->
+								<tbody class="border-top-0">
+									<!-- Table item -->
+									<tr>
+										<td> <h6 class="mb-0">01</h6> </td>
+										<td> <h6 class="mb-0"><a href="#">Finish Us Junior</a></h6> </td>
+										<td> Wedding </td>
+										<td> <h6 class="mb-0 fw-light">April 22 - 23</h6> </td>
+										<td> <div class="badge text-bg-success">Upcoming</div> </td>
+										<td> <div class="badge bg-success bg-opacity-10 text-success">Paid</div> </td> 
+										<td> <a href="#" class="btn btn-sm btn-light mb-0">View</a> </td>
+									</tr>
+
+									<!-- Table item -->
+									<tr>
+										<td> <h6 class="mb-0">02</h6> </td>
+										<td> <h6 class="mb-0"><a href="#">Kingslay Conman</a></h6> </td>
+										<td> Corporate Seminar </td>
+										<td> <h6 class="mb-0 fw-light">Jan 04 - 05</h6> </td>
+										<td> <div class="badge text-bg-success">Done</div> </td>
+										<td> <div class="badge bg-orange bg-opacity-10 text-orange">Paid</div> </td>
+										<td> <a href="#" class="btn btn-sm btn-light mb-0">View</a> </td>
+									</tr>
+
+									<!-- Table item -->
+									<tr>
+										<td> <h6 class="mb-0">03</h6> </td>
+										<td> <h6 class="mb-0"><a href="#">Post Maloan</a></h6> </td>
+										<td> Birthday Party </td>
+										<td> <h6 class="mb-0 fw-light">Feb 24 - 24</h6> </td>
+										<td> <div class="badge text-bg-info">Done</div> </td>
+										<td> <div class="badge bg-info bg-opacity-10 text-info">Paid</div> </td>
+										<td> <a href="#" class="btn btn-sm btn-light mb-0">View</a> </td>
+									</tr>
+
+								</tbody>
+								<!-- Table body END -->
+							</table>
+						</div>
+						<!-- Hotel room list END -->
+					</div>
+					<!-- Card body END -->
+
+					<!-- Card footer START -->
+					<div class="card-footer pt-0">
+						<!-- Pagination and content -->
+						<div class="d-sm-flex justify-content-sm-between align-items-sm-center">
+							<!-- Content -->
+							<p class="mb-sm-0 text-center text-sm-start">Showing 1 to 1 of 3 entries</p>
+							<!-- Pagination -->
+							<nav class="mb-sm-0 d-flex justify-content-center" aria-label="navigation">
+								<ul class="pagination pagination-sm pagination-primary-soft mb-0">
+									<li class="page-item disabled">
+										<a class="page-link" href="#" tabindex="-1">Prev</a>
+									</li>
+									<li class="page-item"><a class="page-link" href="#">1</a></li>
+									<li class="page-item active"><a class="page-link" href="#">2</a></li>
+									<li class="page-item disabled"><a class="page-link" href="#">..</a></li>
+									<li class="page-item"><a class="page-link" href="#">5</a></li>
+									<li class="page-item">
+										<a class="page-link" href="#">Next</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+					<!-- Card footer END -->
+				</div>
+			</div>
+		 </div> 
+		<!-- Booking table END -->
+	</div>
+</section>
+<!-- =======================
+Content END -->
 
         </main>
 
