0261c14ebb0a64a8f7e6e5c151e0402c37bd19ba
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-11 16:13:15 +0300

    Added the admin venue list page

---
+++ admin-venues-list.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Admin Venues List | Ultra Venues</title>
+
+        <!-- Head Content -->
+        <?php include "includes/head-content.php";?>
+
+
+
+    </head>
+
+    <body>
+        <!-- Header START -->
+        <?php include "includes/header-main.php";?>
+        <!-- Header END -->
+
+        <main>
+
+	<!-- Sidebar START -->
+	<nav class="navbar sidebar navbar-expand-xl navbar-light">
+		<!-- Navbar brand for xl START -->
+		<div class="d-flex align-items-center">
+			<a class="navbar-brand" href="index.php">
+				<img class="light-mode-item navbar-brand-item" src="assets/images/logo.svg" alt="logo">
+				<img class="dark-mode-item navbar-brand-item" src="assets/images/logo.svg" alt="logo">
+			</a>
+		</div>
+		<!-- Navbar brand for xl END -->
+		
+		<div class="offcanvas offcanvas-start flex-row custom-scrollbar h-100" data-bs-backdrop="true" tabindex="-1" id="offcanvasSidebar">
+			<div class="offcanvas-body sidebar-content d-flex flex-column pt-4">
+	
+				<!-- Sidebar menu START -->
+				<ul class="navbar-nav flex-column" id="navbar-sidebar">
+					<!-- Menu item -->
+					<li class="nav-item"><a href="admin-dash.php" class="nav-link">Dashboard</a></li>
+                    <hr>
+
+					<!-- Title -->
+					<li class="nav-item ms-2 my-2">Pages</li>
+
+					<!-- Menu item -->
+					<li class="nav-item">
+						<a class="nav-link" data-bs-toggle="collapse" href="#collapsebooking" role="button" aria-expanded="false" aria-controls="collapsebooking">
+						Bookings
+						</a>
+						<!-- Submenu -->
+						<ul class="nav collapse flex-column" id="collapsebooking" data-bs-parent="#navbar-sidebar">
+							<li class="nav-item"> <a class="nav-link" href="admin-booking-list.php">Booking List</a></li>
+							<li class="nav-item"> <a class="nav-link" href="admin-booking-details.php">Booking Details</a></li>
+						</ul>
+					</li>
+	
+					<!-- Menu item -->
+					<li class="nav-item">
+						<a class="nav-link" data-bs-toggle="collapse" href="#collapseguest" role="button" aria-expanded="false" aria-controls="collapseguest">
+						Customers
+						</a>
+						<!-- Submenu -->
+						<ul class="nav collapse flex-column" id="collapseguest" data-bs-parent="#navbar-sidebar">
+							<li class="nav-item"> <a class="nav-link" href="admin-customer-list.php">Customer List</a></li>
+							<li class="nav-item"> <a class="nav-link" href="admin-customer-details.php">Customer Details</a></li>
+						</ul>
+					</li>
+
+					<!-- Menu item -->
+					<li class="nav-item">
+						<a class="nav-link active" data-bs-toggle="collapse" href="#collapseagent" role="button" aria-expanded="false" aria-controls="collapseagent">
+						Venues
+						</a>
+						<!-- Submenu -->
+						<ul class="nav collapse flex-column" id="collapseagent" data-bs-parent="#navbar-sidebar">
+							<li class="nav-item"> <a class="nav-link" href="admin-venues-list.php">Venues List</a></li>
+							<li class="nav-item"> <a class="nav-link" href="admin-venue-details.php">Venue Details</a></li>
+						</ul>
+					</li>
+					
+	
+					<!-- Menu item -->
+					<li class="nav-item"> <a class="nav-link" href="admin-earnings.php">Earnings</a></li>
+	
+					<!-- Menu item -->
+					<li class="nav-item"> <a class="nav-link" href="admin-settings.php">Admin Settings</a></li>
+	
+				</ul>
+				<!-- Sidebar menu end -->
+	
+				<!-- Sidebar footer START -->
+				<div class="d-flex align-items-center justify-content-between text-primary-hover mt-auto p-3">
+					<a class="h6 fw-light mb-0 text-body" href="vendor-login.php" data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Sign out">
+						<i class="fa-solid fa-arrow-right-from-bracket"></i> Log out
+					</a>
+					<a class="h6 mb-0 text-body" href="admin-settings.php" data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Settings">
+						<i class="bi bi-gear-fill"></i>
+					</a>
+				</div>
+				<!-- Sidebar footer END -->
+				
+			</div>
+		</div>
+	</nav>
+	<!-- Sidebar END -->
+
+</main>
+
+    <!-- Footer START -->
+        <?php include "includes/admin-dash-scripts.php";?>
+    <!-- Footer END -->
+
+    </body>
+
+
+</html>
