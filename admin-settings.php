464441f68f71005336d1fac363affd5ab62b098c
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-12 23:36:28 +0300

    Added admin profile section

--- admin-settings.php
+++ admin-settings.php
@@ -284,6 +284,75 @@
 		</nav>
 		<!-- Top bar END -->
 
+		<!-- Page main content START -->
+		<div class="page-content-wrapper p-xxl-4">
+	
+			<!-- Title -->
+			<div class="row">
+				<div class="col-12 mb-4 mb-sm-5">
+					<h1 class="h3 mb-0">Settings</h1>
+				</div>
+			</div>
+
+			<div class="row g-4">
+				<!-- Profile setting -->
+				<div class="col-lg-6">
+					<div class="card shadow">
+						<div class="card-header border-bottom">
+							<h5 class="card-header-title">Admin Profile</h5>
+						</div>
+						<div class="card-body">
+							<!-- Full name -->
+							<div class="mb-3">
+								<label class="form-label">Name</label>
+								<input type="text" class="form-control" value="Ultra Venues Admin" placeholder="Name">
+							</div>
+							<!-- Profile picture -->
+							<div class="mb-3">
+								<label class="form-label">Profile picture</label>
+								<!-- Avatar upload START -->
+								<div class="d-flex align-items-center">
+									<label class="position-relative me-4" for="uploadfile-1" title="Replace this pic">
+										<!-- Avatar place holder -->
+										<span class="avatar avatar-xl">
+											<img id="uploadfile-1-preview" class="avatar-img rounded-circle border border-white border-3 shadow" src="assets/images/user.png" alt="">
+										</span>
+									</label>
+									<!-- Upload button -->
+									<label class="btn btn-sm btn-primary-soft mb-0" for="uploadfile-1">Change</label>
+									<input id="uploadfile-1" class="form-control d-none" type="file">
+								</div>
+								<!-- Avatar upload END -->
+							</div>
+							<!-- Email id -->
+							<div class="mb-3">
+								<label class="form-label">Email Address</label>
+								<input type="email" class="form-control" value="admin@ultra-venues.com" placeholder="Enter your email address">
+							</div>
+							<!-- Mobile number -->
+							<div class="mb-3">
+								<label class="form-label">Mobile number</label>
+								<input type="text" class="form-control" value="+254 712 345 678" placeholder="Enter your mobile number">
+							</div>
+							<!-- Location -->
+							<div class="mb-3">
+								<label class="form-label">Location</label>
+								<input class="form-control" type="text" value="The Cloud">
+							</div>
+							<!-- Website's Birthday -->
+							<div>
+								<label class="form-label">Website Birthday</label>
+								<input type="text" class="form-control flatpickr flatpickr-input" value="1 Feb 2024" placeholder="Enter Ultra-Venue's birth-date" data-date-format="d M Y" readonly="readonly">
+							</div>
+							<!-- Save button -->
+							<div class="d-flex justify-content-end mt-4">
+								<a href="#" class="btn text-secondary border-0 me-2">Discard</a>
+								<a href="#" class="btn btn-primary">Save change</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
 </main>
 
     <!-- Footer START -->
