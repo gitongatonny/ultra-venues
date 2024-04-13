bb4e680c957d67829f67138cb0ffee05c1fb6802
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-13 04:56:07 +0300

    Added customer booking history section

--- admin-customer-details.php
+++ admin-customer-details.php
@@ -284,6 +284,203 @@
 		</nav>
 		<!-- Top bar END -->
 
+		<!-- Page main content START -->
+		<div class="page-content-wrapper p-xxl-4">
+	
+			<div class="card shadow">
+				<!-- Card header -->
+				<div class="card-header border-bottom d-sm-flex justify-content-between align-items-center">
+					<!-- Avatar and content -->
+					<div class="d-flex align-items-center flex-shrink-0">
+						<!-- Avatar -->
+						<div class="avatar avatar-lg">
+							<img class="avatar-img rounded-circle" src="assets/images/user.png" alt="avatar">
+						</div>
+						<!-- Info -->
+						<div class="ms-3">
+							<h5 class="mb-0">Finish Us Junior</h5></h5>
+						</div>
+					</div>
+
+					<div class="d-block d-lg-flex gap-lg-5 flex-wrap mt-3 mt-lg-0">
+						<!-- Email address -->
+						<h6 class="mb-2 mb-lg-0"><i class="bi bi-envelope-fill fa-fw me-1"></i>Email Address:<a href="#" class="fw-normal"> finny@real.com</a></h6>
+						<!-- Email address -->
+						<h6 class="mb-0"><i class="bi bi-headset fa-fw me-1"></i>Contact no:<a href="#" class="fw-normal"> +254-700-123-555</a></h6>
+					</div>
+				</div>
+				
+				<!-- Card body -->
+				<div class="card-body">
+					
+					<!-- Title -->
+					<h5 class="mb-3">Booking History</h5>
+					
+					<!-- Table head -->
+					<div class="bg-light rounded p-3 d-none d-lg-block">
+						<div class="row row-cols-6 justify-content-between g-4">
+							<div class="col"><h6 class="mb-0">Venue Name</h6></div>
+							<div class="col"><h6 class="mb-0">Venue Type</h6></div>
+							<div class="col"><h6 class="mb-0">Start Date</h6></div>
+							<div class="col"><h6 class="mb-0">End Date</h6></div>
+							<div class="col"><h6 class="mb-0">Action</h6></div>
+						</div>
+					</div>
+
+					<!-- Table data -->
+					<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-6 g-2 g-sm-4 align-items-md-center justify-content-between border-bottom px-2 py-4">
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Venue Name:</small>
+							<div class="d-flex align-items-center">
+								<!-- Image -->
+								<div class="w-80px flex-shrink-0">
+									<img src="assets/images/mt-kenya.jpg" class="rounded" alt="">
+								</div>
+								<!-- Title -->
+								<h6 class="mb-0 ms-2">Green Hills Hotel</h6>
+							</div>
+						</div>	
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Venue Type:</small>
+							<h6 class="mb-0 fw-normal">Hotel</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Start Date:</small>
+							<h6 class="text-success mb-0">22 Nov 2024</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">End Date:</small>
+							<h6 class="text-success mb-0">23 Nov 2024</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col"><a href="#" class="btn btn-sm btn-light mb-0">View</a></div>
+					</div>
+
+					<!-- Table data -->
+					<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-6 g-2 g-sm-4 align-items-md-center justify-content-between border-bottom px-2 py-4">
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Venue Name:</small>
+							<div class="d-flex align-items-center">
+								<!-- Image -->
+								<div class="w-80px flex-shrink-0">
+									<img src="assets/images/mt-kenya.jpg" class="rounded" alt="">
+								</div>
+								<!-- Title -->
+								<h6 class="mb-0 ms-2">Green Hills Hotel</h6>
+							</div>
+						</div>	
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Venue Type:</small>
+							<h6 class="mb-0 fw-normal">Hotel</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Start Date:</small>
+							<h6 class="text-success mb-0">22 Nov 2024</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">End Date:</small>
+							<h6 class="text-success mb-0">23 Nov 2024</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col"><a href="#" class="btn btn-sm btn-light mb-0">View</a></div>
+					</div>
+
+					<!-- Table data -->
+					<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-6 g-2 g-sm-4 align-items-md-center justify-content-between border-bottom px-2 py-4">
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Venue Name:</small>
+							<div class="d-flex align-items-center">
+								<!-- Image -->
+								<div class="w-80px flex-shrink-0">
+									<img src="assets/images/mt-kenya.jpg" class="rounded" alt="">
+								</div>
+								<!-- Title -->
+								<h6 class="mb-0 ms-2">Green Hills Hotel</h6>
+							</div>
+						</div>	
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Venue Type:</small>
+							<h6 class="mb-0 fw-normal">Hotel</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Start Date:</small>
+							<h6 class="text-success mb-0">22 Nov 2024</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">End Date:</small>
+							<h6 class="text-success mb-0">23 Nov 2024</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col"><a href="#" class="btn btn-sm btn-light mb-0">View</a></div>
+					</div>
+					
+					<!-- Table data -->
+					<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-6 g-2 g-sm-4 align-items-md-center justify-content-between border-bottom px-2 py-4">
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Venue Name:</small>
+							<div class="d-flex align-items-center">
+								<!-- Image -->
+								<div class="w-80px flex-shrink-0">
+									<img src="assets/images/mt-kenya.jpg" class="rounded" alt="">
+								</div>
+								<!-- Title -->
+								<h6 class="mb-0 ms-2">Green Hills Hotel</h6>
+							</div>
+						</div>	
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Venue Type:</small>
+							<h6 class="mb-0 fw-normal">Hotel</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">Start Date:</small>
+							<h6 class="text-success mb-0">22 Nov 2024</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col">
+							<small class="d-block d-lg-none">End Date:</small>
+							<h6 class="text-success mb-0">23 Nov 2024</h6>
+						</div>
+
+						<!-- Data item -->
+						<div class="col"><a href="#" class="btn btn-sm btn-light mb-0">View</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Page main content END -->
+	</div>
+	<!-- Page content END -->
+
 </main>
 
     <!-- Footer START -->
