5541dfa9c58b468cb0bd4c4359d3cc604bac7554
FredrickMburu <mburu.fredrick21@students.dkut.ac.ke>
2024-08-07 05:33:01 +0300

    minor fixes

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("ultra-venues", "root", "", {
-	host: "localhost",
-	dialect: "mysql",
-});
-
-module.exports = sequelize;
