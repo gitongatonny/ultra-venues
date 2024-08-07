5b34d03ceca2e42f4c18ac88c1cb6f82f73b0806
FredrickMburu <109967236+FredrickMburu@users.noreply.github.com>
2024-08-07 09:36:26 +0300

    minor fixes

--- Recommendation system/app.py
+++ Recommendation system/app.py
@@ -1,25 +1,3 @@
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-# from recommendations import get_venue_recommendations, get_popular_venues
-
-# app = Flask(__name__)
-# CORS(app)  # Enable CORS for all routes
-
-# @app.route('/recommend', methods=['GET'])
-# def recommend():
-#     venue_id = request.args.get('id')
-#     n_recommendations = int(request.args.get('n', 5))
-    
-#     if venue_id:
-#         venue_id = int(venue_id)
-#         recommendations = get_venue_recommendations(venue_id, n_recommendations)
-#     else:
-#         recommendations = get_popular_venues(n_recommendations)
-    
-#     return jsonify(recommendations.to_dict(orient='records'))
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from recommendations import get_venue_recommendations, get_popular_venues
